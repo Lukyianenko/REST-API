4bddc47032197cae6062f62ac611175209fb2847
Lukyianenko <110479686+Lukyianenko@users.noreply.github.com>
2023-06-06 16:06:09 +0300

    add resize avatars

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -8,17 +8,19 @@ const avatarsDir = path.join(__dirname, "../../", "public", "avatars" );
 const updateAvatar = async (req, res) => {
     const {_id} = req.user;
     const {path: tempUpload, originalname} = req.file;
-    const filename = `${_id}_${originalname}`
-    const resultUpload = path.join(avatarsDir, filename)
+    const filename = `${_id}_${originalname}`;
+    const resultUpload = path.join(avatarsDir, filename);
     await fs.rename(tempUpload, resultUpload);
-    Jimp.read(resultUpload).then((image) => {
-        image
-          .resize(250, 250)
-          .write(filename);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+
+    await Jimp.read(resultUpload).then((image) => {
+      return image
+        .resize(250, 250)
+        .write(resultUpload) 
+   })
+    .catch((err) => {
+      console.error(err);
+    });
+    
     const avatarURL = path.join("avatars", filename)
         await User.findByIdAndUpdate(_id, {avatarURL})
 
